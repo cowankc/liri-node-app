bb85e93fd5ae94824d1718f523c5a54759bfcd6d
cowankc <cowankc92@live.com>
2019-10-05 20:59:13 -0500

    finished dowhatitsays function and ready to launch

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 let axios = require("axios");
 let fs = require("fs");
 let Spotify = require('node-spotify-api');
-
+let moment = require('moment');
 let Search = function() {
     let divider = "\n------------------------------------------------------------\n\n"
     this.findBand = function(band) {
@@ -12,7 +12,7 @@ let Search = function() {
             let bandData = [
                 "Venue: " + data[i].venue.name,
                 "City: " + data[i].venue.city,
-                "Date: " + data[i].datetime,
+                "Date: " + moment(data[i].datetime).format("MM/DD/YYYY"),
             ].join("\n\n");
             console.log(bandData)
             fs.appendFile("log.txt", bandData + divider, function(err) {
@@ -22,7 +22,25 @@ let Search = function() {
         })
     }
     this.findMovie = function(movie) {
-        let URL = "http://www.omdbapi.com/?apikey=trilogy&t=" + movie;
+        if (movie === "") {
+        let URL = "http://www.omdbapi.com/?apikey=trilogy&t=" + "Mr. Nobody.";
+        axios.get(URL).then(function(response){
+            let movieData = [
+                "Title: " + response.data.Title,
+                "Release Date: " + response.data.Released,
+                "IMBD Rating: " + response.data.imdbRating,
+                "Rotten Tomato Rating: " + response.data.Ratings[1].Value,
+                "Country Produced: " + response.data.Country,
+                "Language: " + response.data.Language,
+                "Plot: " + response.data.Plot,
+                "Actors: " + response.data.Actors,
+            ].join("\n\n");
+            console.log(movieData)
+            fs.appendFile("log.txt", movieData + divider, function(err) {
+                if (err) throw err;
+            });
+        })
+        } else { let URL = "http://www.omdbapi.com/?apikey=trilogy&t=" + movie;
         axios.get(URL).then(function(response){
             let movieData = [
                 "Title: " + response.data.Title,
@@ -40,12 +58,29 @@ let Search = function() {
             });
         })
     }
+    }
     this.findSong = function(song) {
         let spotify = new Spotify({
             id: "cb5fa19fa5f8444e835b6d3dc2d63a3f",
             secret: "505ee097257444e4bd42e1789e62b101",
         })
-        spotify.search({ type: 'track', query: song }, function(err, data) {
+        if (song === "") {
+            spotify.search({ type: 'track', query: "The Sign" }, function(err, data) {
+                if (err) {
+                  return console.log('Error occurred: ' + err);
+                }
+                let songData = [
+                    "Artist(s): " + data.tracks.items[0].artists[0].name,
+                    "Song Name: " + data.tracks.items[0].name,
+                    "Song Preview: " + data.tracks.items[0].preview_url,
+                    "Album Name: " + data.tracks.items[0].album.name,
+                ].join("\n\n");
+                console.log(songData); 
+                fs.appendFile("log.txt", songData + divider, function(err) {
+                    if (err) throw err;
+                });
+            })
+        } else {spotify.search({ type: 'track', query: song }, function(err, data) {
             if (err) {
               return console.log('Error occurred: ' + err);
             }
@@ -56,12 +91,12 @@ let Search = function() {
                 "Album Name: " + data.tracks.items[0].album.name,
             ].join("\n\n");
             console.log(songData); 
+            fs.appendFile("log.txt", songData + divider, function(err) {
+                if (err) throw err;
+            });
         })
     }
+    }
 }
-
-let newMovie = new Search()
-
-newMovie.findSong("Black Betty")
 
 module.exports = Search
--- liri.js
+++ liri.js
@@ -1,4 +1,44 @@
 require("dotenv").config();
 let keys = require("./keys.js");
 let Search = require("./api.js")
-var spotify = new Spotify(keys.spotify);
+let liriSearch = new Search()
+let command = process.argv[2]
+let term = process.argv.slice(3).join(" ")
+let fs = require("fs");
+
+let doWhatItSays = function () {
+    fs.readFile("random.txt", 'utf8', function(err, data) {
+    dataArray = data.split(",")
+    console.log(dataArray[0])
+    console.log(dataArray[1])
+    if (dataArray[0] === "spotify-this-song") {
+        liriSearch.findSong(dataArray[1])
+    }
+    if (dataArray[0] === "concert-this") {
+        liriSearch.findBand(dataArray[1])
+    }
+    if (dataArray[0] === "movie-this") {
+        liriSearch.findMovie(dataArray[1])
+    }
+    })
+}
+
+if (command === "concert-this") {
+    liriSearch.findBand(term)
+}
+
+if (command === "spotify-this-song") {
+    liriSearch.findSong(term)
+}
+
+if (command === "spotify-this-song" && term === "") {
+    liriSearch.findSong("The Sign")
+}
+
+if (command === "movie-this") {
+    liriSearch.findMovie(term)
+}
+
+if (command === "do-what-it-says") {
+    doWhatItSays()
+}
